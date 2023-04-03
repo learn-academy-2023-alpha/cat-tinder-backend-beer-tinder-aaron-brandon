d64dbd0feb062b5171eaa59509256a0bd04dbd3e
BxRay95 <bray387@outlook.com>
2023-04-03 14:44:47 -0700

    add resource, seed and cors for backend structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv' 
+CSV.foreach(Rails.root.join('lib/beer-seed.csv'), headers: true) do |row|
+    Beer.create({
+        name: row["name"],
+        brewery: row["brewery"], 
+        abv: row["abv"], 
+        ibu: row["ibu"], 
+        primary_flavor: row["primary_flavor"], 
+        secondary_flavor: row["secondary_flavor"],
+        color: row["color"],
+        variety: row["variety"]
+    })
+end
+
